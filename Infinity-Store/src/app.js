3ea1b02c2611c42de425f40c96da56aa8a023b30
nacho522 <ignaciocosta1234@gmail.com>
2022-03-09 20:28:59 -0300

    register

--- Infinity-Store/src/app.js
+++ Infinity-Store/src/app.js
@@ -8,3 +8,5 @@ app.use(express.static(publicPath));
 app.listen( port, () => console.log("Servidor corriendo en el puerto " + port));
 
 app.get("/productCart", (req, res) => {res.sendFile(path.join(__dirname, "views", "productCart.html"))});
+app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "views", "login.html"))})
+app.get("/register", (req, res) => {res.sendFile(path.join(__dirname, "views", "register.html"))})
