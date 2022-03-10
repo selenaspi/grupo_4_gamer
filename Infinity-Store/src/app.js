f2bbde45d17bce934d77e2cb5401db0ecf042be2
nacho522 <ignaciocosta1234@gmail.com>
2022-03-10 20:56:55 -0300

    agrego estilos

--- Infinity-Store/src/app.js
+++ Infinity-Store/src/app.js
@@ -8,13 +8,11 @@ app.use(express.static(publicPath));
 app.listen( port, () => console.log("Servidor corriendo en el puerto " + port));
 
 app.get("/productCart", (req, res) => {res.sendFile(path.join(__dirname, "views", "productCart.html"))});
-<<<<<<< HEAD
+
 app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "views", "login.html"))})
 app.get("/register", (req, res) => {res.sendFile(path.join(__dirname, "views", "register.html"))})
-=======
 
 
 app.get("/productDetails", (req, res) => {res.sendFile(path.join(__dirname, "views", "productDetails.html"))})
 
 app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"))})
->>>>>>> 3b4fd12fbd95159f6f909789578730f846172421
