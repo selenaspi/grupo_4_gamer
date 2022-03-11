f3f888c5c927703925abe807e972568c43a72856
Selena <ramosselena9@gmail.com>
2022-03-11 06:32:16 -0300

    Agrego estilos a carrito de compras

--- Infinity-Store/src/app.js
+++ Infinity-Store/src/app.js
@@ -8,18 +8,7 @@ app.use(express.static(publicPath));
 app.listen( port, () => console.log("Servidor corriendo en el puerto " + port));
 
 app.get("/productCart", (req, res) => {res.sendFile(path.join(__dirname, "views", "productCart.html"))});
-<<<<<<< HEAD
-
-=======
->>>>>>> 4681b76ed0acc590b76a62c2119b0e459fc2b167
 app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "views", "login.html"))})
 app.get("/register", (req, res) => {res.sendFile(path.join(__dirname, "views", "register.html"))})
-
-
 app.get("/productDetails", (req, res) => {res.sendFile(path.join(__dirname, "views", "productDetails.html"))})
-
-<<<<<<< HEAD
 app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"))})
-=======
-app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"))})
->>>>>>> 4681b76ed0acc590b76a62c2119b0e459fc2b167
