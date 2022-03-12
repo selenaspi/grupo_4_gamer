45022546ff1508578c746528ab68a706078c4b31
Selena Ramos <ramosselena9@gmail.com>
2022-03-12 02:24:28 -0300

    Estilos carrito, adaptamiento desktop y agrego wireframes tablet

--- Infinity-Store/src/app.js
+++ Infinity-Store/src/app.js
@@ -8,22 +8,7 @@ app.use(express.static(publicPath));
 app.listen( port, () => console.log("Servidor corriendo en el puerto " + port));
 
 app.get("/productCart", (req, res) => {res.sendFile(path.join(__dirname, "views", "productCart.html"))});
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> cfb790e270ca9646672211a4a70e9450ba2312eb
 app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "views", "login.html"))})
 app.get("/register", (req, res) => {res.sendFile(path.join(__dirname, "views", "register.html"))})
 app.get("/productDetails", (req, res) => {res.sendFile(path.join(__dirname, "views", "productDetails.html"))})
-<<<<<<< HEAD
-
-
 app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"))})
-
-app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"))})
-
-=======
-app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"))})
->>>>>>> cfb790e270ca9646672211a4a70e9450ba2312eb
