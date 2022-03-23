2ef844dd48c1ce6b2c1b4283a566df24cb016fb9
Selena <ramosselena9@gmail.com>
2022-03-23 20:23:05 -0300

    App.set

--- Infinity-Store/src/app.js
+++ Infinity-Store/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require ('path');
 const app = express(); 
+app.set("views engine", "ejs");
 const mainRouter = require('./routes/main');
 const productCartRouter = require('./routes/productCart');
 const loginRouter = require('./routes/login');
