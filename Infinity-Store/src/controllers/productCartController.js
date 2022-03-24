5949e4ca8f14e113270c3ad0776d6b8902ddc77c
Selena Ramos <ramosselena9@gmail.com>
2022-03-24 02:47:50 -0300

    Arreglo de ejs

--- Infinity-Store/src/controllers/productCartController.js
+++ Infinity-Store/src/controllers/productCartController.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const controller = {
-    mostrarCarrito : (req, res) => {res.render("products/productCart.ejs")}
+    mostrarCarrito : (req, res) => {res.render("products/productCart")}
 }
 
 module.exports = controller;
