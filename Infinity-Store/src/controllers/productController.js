bfefa411768d9ffd220d859b87941931a5286ea0
NicoGerez <gerez.nicolas999@gmail.com>
2022-03-29 20:24:04 -0300

    ProductDetails.ejs

--- Infinity-Store/src/controllers/productController.js
+++ Infinity-Store/src/controllers/productController.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 const controller = {
-    mostrarDetalleProducto : (req, res) => {res.render("products/productDetails")},
+    mostrarDetalleProducto : (req, res) => {
+        let productosSimilares = {
+            descripcion :"Productos Similares",
+                precio : "$10000",  
+        };
+        res.render("products/productDetails")},
     crearProducto : (req,res) => {res.render("products/productCreationEdition", {existe : false, name : null, descripcion : null, foto : null, categoria : null, precio : null})},
     editarProducto : (req, res) => {res.render("products/productCreationEdition", {existe : true, name : "Compu gamer", descripcion : "Aquí una descripción del producto", categoria : "teclado", foto : null})}
+
+
+
 }
 
+
 module.exports = controller;
+
