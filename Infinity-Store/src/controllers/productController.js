00f2b4aa2da7e1b52e9b27bc8d7fed088f691635
NicoGerez <gerez.nicolas999@gmail.com>
2022-03-29 21:51:37 -0300

    solucion ProductDetails.ejs

--- Infinity-Store/src/controllers/productController.js
+++ Infinity-Store/src/controllers/productController.js
@@ -4,9 +4,9 @@ const controller = {
     mostrarDetalleProducto : (req, res) => {
         let productosSimilares = {
             descripcion :"Productos Similares",
-                precio : "$10000",  
+            precio : "$10000",  
         };
-        res.render("products/productDetails")},
+        res.render("products/productDetails",{productosSimilares})},
     crearProducto : (req,res) => {res.render("products/productCreationEdition", {existe : false, name : null, descripcion : null, foto : null, categoria : null, precio : null})},
     editarProducto : (req, res) => {res.render("products/productCreationEdition", {existe : true, name : "Compu gamer", descripcion : "Aquí una descripción del producto", categoria : "teclado", foto : null})}
 
