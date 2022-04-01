10ae9d4302fce5e5a6ff6e06dc29f26a7559c206
NicoGerez <gerez.nicolas999@gmail.com>
2022-03-31 22:24:45 -0300

    actualizacion productDetails

--- Infinity-Store/src/controllers/productController.js
+++ Infinity-Store/src/controllers/productController.js
@@ -20,7 +20,7 @@ const controller = {
             precio : "$10000",  
         };
         res.render("products/productDetails",{productos: listaProductos,productosSimilares})},
-    crearProducto : (req,res) => {res.render("products/productCreationEdition", {existe : false, name : null, descripcion : null, foto : null, categoria : null, precio : null})},
+    crearProducto : (req,res) => {res.render("products/productCreationEdition", {existe : false, name : null, descripcion : null, foto : null, categoria : null, precio : null} )},
     editarProducto : (req, res) => {res.render("products/productCreationEdition", {existe : true, name : "Compu gamer", descripcion : "Aquí una descripción del producto", categoria : "teclado", foto : null})}
 
 
