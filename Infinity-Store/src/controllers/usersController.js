0782e8afabf18bf6b687ef8daeb61721525d2ce3
martin <martinmagalhaes1@gmail.com>
2022-04-08 09:49:58 -0300

    guardando carpeta user

--- Infinity-Store/src/controllers/usersController.js
+++ Infinity-Store/src/controllers/usersController.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const controller = {
+    
     mostrarLogin : (req, res) => {res.render("users/login")},
 
-    mostrarRegistro : (req, res) => {res.render("users/register")}
+    mostrarRegistro : (req, res) => {res.render("users/register")},
+// Root -- show all users 
+index: (req, res) => {
+    return res.render("users", {users});
+},
+
+
 }
 
 module.exports = controller;
--- Infinity-Store/src/routes/users.js
+++ Infinity-Store/src/routes/users.js
@@ -1,6 +1,8 @@
+// ************* Require's ******************
 const express = require("express");
 const router = express.Router();
 const users = require('../controllers/usersController.js')
+
 
 router.get("/login", users.mostrarLogin);
 router.get("/register", users.mostrarRegistro);
