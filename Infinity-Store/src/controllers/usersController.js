4f3dcbba271089485a39de62fd40cc3e3d10f343
martin <martinmagalhaes1@gmail.com>
2022-04-08 10:44:43 -0300

    crud usuarios

--- Infinity-Store/src/controllers/usersController.js
+++ Infinity-Store/src/controllers/usersController.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const usersFilePath = path.join(__dirname, '../database/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const controller = {
@@ -12,7 +12,38 @@ index: (req, res) => {
     return res.render("users", {users});
 },
 
+// Detail - Detail from one user 
 
+detail : (req,res) => {
+    const id = req.params.id;
+    const user = users.find(user => user.id == id);
+    return res.render("detail", {user});
+},
+
+// Create - form to create
+create : (req,res) =>{
+    return res.render("register");
+},
+
+// create - method to store
+store:(req,res)=>{
+    return res.send("Usuario creado");
+},
+
+edit: (req,res)=>{
+    const id = req.params.id;
+    const user = user.find(user => user.id == id);
+	return res.render("login ", {user});
+},
+// Update - Method to update
+update: (req, res) => {
+    return res.send("Usuario editado");
+},
+
+// Delete - Delete one product from DB
+destroy : (req, res) => {
+    return res.send("Usuario eliminado");
 }
+};
 
 module.exports = controller;
--- Infinity-Store/src/routes/users.js
+++ Infinity-Store/src/routes/users.js
@@ -1,7 +1,27 @@
-// ************* Require's ******************
+// REQUIRE'S
 const express = require("express");
 const router = express.Router();
-const users = require('../controllers/usersController.js')
+
+// CONTROLLER REQUIRE
+const usersController = require('../controllers/usersController.js')
+
+// GET ALL USERS 
+router.get("/", usersController.index);
+
+// CREATE ONE USER 
+router.get("/create", usersController.create);
+router.post("/", usersController.store);
+
+// GET ONE USER
+router.get("/id:/", usersController.detail);
+
+/*** EDIT ONE USER ***/ 
+router.get('/edit/:id', usersController.edit); 
+router.put('/:id', usersController.update); 
+
+
+/*** DELETE ONE USER***/ 
+router.delete('/:id', usersController.destroy);
 
 
 router.get("/login", users.mostrarLogin);
