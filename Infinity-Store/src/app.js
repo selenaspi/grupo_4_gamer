df15cc8714a6542157410a7f12e38859b2c9b852
nacho522 <ignaciocosta1234@gmail.com>
2022-04-07 22:50:47 -0300

    agrego líneas de códigos para override

--- Infinity-Store/src/app.js
+++ Infinity-Store/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require ('path');
 const app = express(); 
+const methodOverride = require('method-override');
 
 app.set("view engine", "ejs");
 app.set('views', './src/views');
@@ -24,4 +25,5 @@ app.use("/users",usersRouter);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
