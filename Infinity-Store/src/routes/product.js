da19fce0ab71c25dbfcdd1657a8791c2b4fd3243
Selena <ramosselena9@gmail.com>
2022-04-12 06:25:38 -0300

    Formulario de edicióon de producto

--- Infinity-Store/src/routes/product.js
+++ Infinity-Store/src/routes/product.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const productController = require('../controllers/productController.js')
 
-router.get("/product-creation",productController.formCreation);
-router.get("/product-edition", productController.editarProducto);
+//Formulario de creación de productos
+router.get("/create",productController.formCreation);
+router.post('/', productController.crearProducto);
+
+//Formualario de edición
+router.get("/:id/edit", productController.formEdition);
+router.put("/:id", productController.editarProducto)
+
+
 router.get("/details/:id", productController.mostrarDetalleProducto);
-router.post('/creacion', productController.crearProducto);
 module.exports = router;
