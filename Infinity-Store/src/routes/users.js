625c7cc0c4bb21ec4b90a7dbf52e99dddc51acdf
nacho522 <ignaciocosta1234@gmail.com>
2022-04-15 10:35:36 -0300

    configuro multer para usuarios

--- Infinity-Store/src/routes/users.js
+++ Infinity-Store/src/routes/users.js
@@ -1,12 +1,28 @@
-// REQUIRE'S
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const multer = require("multer");
+
+
+const productController = require('../controllers/productController.js')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,path.resolve(__dirname,"../../public/images/usuarios"))
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 // CONTROLLER REQUIRE
 const usersController = require('../controllers/usersController')
 
-// CREATE ONE USER 
-router.post("/register", usersController.store);
+// Formulario de creación de usuarios
+router.post("/register",upload.single("photo"), usersController.store);
 router.get("/register", usersController.mostrarRegistro);
 
 // GET ONE USER
