5e94a683dfa56d2872e2972a9b95f5221ffc7c7d
Selena Ramos <ramosselena9@gmail.com>
2022-04-19 22:10:55 -0300

    arreglo edit

--- Infinity-Store/src/controllers/productController.js
+++ Infinity-Store/src/controllers/productController.js
@@ -20,7 +20,7 @@ const controller = {
         res.render("products/productDetails", { similares: listaProductos, detalle: productsList[idProducto - 1], categoryList })
     },
 
-    formCreation: (req, res) => { 
+    formCreation: (req, res) => {
         res.render("products/productCreationEdition", {
             metodo: "POST",
             ruta: "",
@@ -30,22 +30,22 @@ const controller = {
 
     crearProducto: (req, res) => {
         let ids = [];
-        productos.forEach(producto => {ids.push(producto.id)});
+        productos.forEach(producto => { ids.push(producto.id) });
         let maxId = Math.max(...ids);
 
-        let offSaleOn = req.body.checkDescuento === "on"? true: false;
+        let offSaleOn = req.body.checkDescuento === "on" ? true : false;
 
-        let discountUpdate = offSaleOn? req.body.discount : 0; 
+        let discountUpdate = offSaleOn ? req.body.discount : 0;
 
-        let newProduct =     {
+        let newProduct = {
             id: maxId + 1,
             name: req.body.name,
             idCategory: req.body.category,
             description: req.body.description,
             data_sheet: [],
-            image:req.file.filename,
-            color : [],
-            price : req.body.price,
+            image: req.file.filename,
+            color: [],
+            price: req.body.price,
             offSale: offSaleOn,
             discount: discountUpdate,
             stock: req.body.stock,
@@ -53,7 +53,7 @@ const controller = {
 
         productos.push(newProduct);
         fs.writeFileSync(productosfilePath, JSON.stringify(productos));
-        
+
         res.redirect('/');
     },
 
@@ -70,12 +70,12 @@ const controller = {
         res.render("products/productCreationEdition", {
             metodo: "PUT",
             ruta: req.params.id + "?_method=PUT",
-            producto: productoElegido, 
-            category : categoryList
+            producto: productoElegido,
+            categoryList
         })
     },
 
-    editarProducto : (req, res) => {
+    editarProducto: (req, res) => {
 
         let listaNuevaProductos = [];
 
@@ -85,19 +85,19 @@ const controller = {
             }
         });
 
-        let offSaleOn = req.body.checkDescuento === "on"? true: false;
+        let offSaleOn = req.body.checkDescuento === "on" ? true : false;
 
-        let productUpdate =     {
+        let productUpdate = {
             id: req.params.id,
             name: req.body.name,
             idCategory: req.body.category,
             description: req.body.description,
             data_sheet: [],
-            image : "",
-            color : [],
-            price : req.body.price,
+            image: "",
+            color: [],
+            price: req.body.price,
             offSale: offSaleOn,
-            discount: offSaleOn === true? req.body.discount : 0,
+            discount: offSaleOn === true ? req.body.discount : 0,
             stock: req.body.stock
         }
 
@@ -106,17 +106,19 @@ const controller = {
 
         res.redirect('/');
     },
-    allProducts: (req,res) => {
- 
-        res.render("products/allProducts",{similares: productsList, categoryList})
-    },   productDelete : (req, res) => {
+    allProducts: (req, res) => {
+
+        res.render("products/allProducts", { similares: productsList, categoryList })
+    },
+    productDelete: (req, res) => {
         const id = req.params.id;
-        productsList = products.filter( productos => productos.id !=  id);
+        productsList = products.filter(productos => productos.id != id);
         fs.writeFileSync(productosfilePath, JSON.stringify(productsList));
-        
-        res.redirect('/')}
-    
-    
+
+        res.redirect('/')
+    }
+
+
 }
 
 
