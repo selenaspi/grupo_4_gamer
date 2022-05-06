3d1248a9928a75528fb0fcb19671ea3f69ba44f4
Selena Ramos <ramosselena9@gmail.com>
2022-05-06 01:29:01 -0300

    Implementación de carpeta models con los métodos correspondientes, adaptación de controladores, arreglos varios

--- Infinity-Store/src/routes/users.js
+++ Infinity-Store/src/routes/users.js
@@ -3,46 +3,39 @@ const router = express.Router();
 const path = require("path");
 const multer = require("multer");
 
-
 const productController = require('../controllers/productController.js')
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null,path.resolve(__dirname,"../../public/images/usuarios"))
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, "../../public/images/usuarios"))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+})
+
+const upload = multer({ storage: storage })
 
 // CONTROLLER REQUIRE
 const usersController = require('../controllers/usersController')
 
-// Formulario de creación de usuarios
-router.post("/register",upload.single("image"), usersController.store);
-router.get("/register", usersController.mostrarRegistro);
+// Formulario de creación de usuarios - CREATE
+router.get("/register", usersController.register);
+router.post("/register", upload.single("image"), usersController.store);
 
-// GET ONE USER
-router.get("/id:/", usersController.detail);
+//Perfil de usuario - READ
+router.get("/profileUser", usersController.profileUser);
 
-//-----------------------------------------------------------------------------------//
-//Formulario de edición
-router.get('/:id/:edit', usersController.mostrarformulariodeedicion); 
-router.put('/:id',upload.single("image"),usersController.edit); 
-//----------------------------------------------------------------------------------//
+//Formulario de edición - UPDATE
+router.get('/:id/edit', usersController.edition);
+router.put('/:id', upload.single("image"), usersController.edit);
 
-/*** DELETE ONE USER***/ 
+// DELETE ONE USER - DELETE
 router.get("/:id/delete", usersController.mostrarBorradoDeUsuario);
-router.delete("/:id/delete", usersController.delete);
+router.delete("/:id", upload.single("image"), usersController.deleteUser);
 
-/*** LOGUIN ***/
+/*** LOGIN ***/
 router.get("/login", usersController.mostrarLogin);
-
-/*** PROFILE USER ***/
-router.get("/profileUser",usersController.mostrarProfileUser);
-
 
 module.exports = router;
