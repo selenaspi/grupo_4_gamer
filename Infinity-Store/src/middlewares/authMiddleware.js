df80c01c2b9a2b37334483b9ea64710fa644df04
Nico Gerez <gerez.nicolas999@gmail.com>
2022-06-06 20:42:03 -0300

    backup login

--- Infinity-Store/src/middlewares/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-function authMiddleware(req, res, next) {
-	if (!req.session.userLogged) {
-		return res.redirect('/users/login');
-	}
-	next();
-}
-
-module.exports = authMiddleware;
--- Infinity-Store/src/routes/productCart.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productCartController = require("../controllers/productCartController.js")
-
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.get("/", authMiddleware, productCartController.mostrarCarrito);
-
-module.exports = router;
