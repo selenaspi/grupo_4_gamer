abbdb6860b415654f286dec2deec161a8b14ccfb
martin <martinmagalhaes1@gmail.com>
2022-06-10 20:21:50 -0300

    product_images

--- Infinity-Store/src/database/models/Product_image.js
+++ Infinity-Store/src/database/models/Product_image.js
@@ -1,50 +1,50 @@
-// module.exports = (sequelize, DataTypes) => {
-//     let cols = {
-//         id:{
-//             type : DataTypes.INTEGER(10),
-//         autoIncrement : true,
-//         allowNull : false, 
-//         primaryKey : true 
-//         },
-//         created_at : {
-// 			type : DataTypes.DATE,
-// 			defaultValue : DataTypes.NOW,
-// 			allowNull : false
-// 		}, 
-//         name : {
-// 			type: DataTypes.TEXT,
-// 			allowNull : false
-// 		},
-//         alta : {
-// 			type : DataTypes.TINYINT(1),
-// 			allowNull : false,
-// 			defaultValue : 1
-// 		}
-//     }
-//     let config = {
-//         tableName : "product_image",
-// 		charset: 'utf8',
-// 		collate: 'utf8_unicode_ci',
-// 		timestamps: true,
-// 		createdAt: "created_at",
-//     }
-//     const Product_image = sequelize.define("Product_image", cols, config)
-   
-//     Product_image.associate = function(models) {
-// 		Product_image.belongsTo(models.Product, {
-// 			as: "product",
-// 			foreignKey: "product_id"
-// 		})
-//         Product_image.hasMany(models.Product, {
-// 			as: "Products",
-// 			foreignKey: "product_id"
-// 		})
-//         Product_image.belongsToMany(models.Product_image, {
-// 			as: "Product_image",
-//             through: "products",
-// 			foreignKey: "product_id",
-// 		})
-//     }
-//         return Product_image;
-// }
+module.exports = (sequelize, DataTypes) => {
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER(10),
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        alta: {
+            type: DataTypes.TINYINT(1),
+            allowNull: false,
+            defaultValue: 1
+        }
+    }
 
+    let config = {
+        tableName: "product_image", charset: 'utf8',
+        collate: 'utf8_unicode_ci',
+        charset: 'utf8',
+        timestamps: true,
+        createdAt: "created_at",
+    }
+
+    const Product_image = sequelize.define("Product_image", cols, config)
+
+    Product_image.associate = function (models) {
+        Product_image.belongsTo(models.Product, {
+            as: "product",
+            foreignKey: "product_id"
+        })
+        Product_image.hasMany(models.Product, {
+            as: "roducts",
+            foreignKey: "product_id"
+        })
+        Product_image.belongsToMany(models.Product_image, {
+            as: "Product_image", through: "products",
+            foreignKey: "product_id",
+        })
+    }
+    return Product_image;
+}
