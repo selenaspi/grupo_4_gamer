775b3ec702315939ecfa536d306c28e338fa916a
Selena Ramos <ramosselena9@gmail.com>
2022-06-19 05:40:22 -0300

    ..

--- Infinity-Store/src/middlewares/validateRegisterMiddleware.js
+++ Infinity-Store/src/middlewares/validateRegisterMiddleware.js
@@ -3,19 +3,19 @@ const db = require('../database/models');
 const path = require('path');
 const { body } = require('express-validator');
 
-const validator  = [
+const validator = [
 	body('name').notEmpty().withMessage("El nombre es obligatorio").isLength({ min: 2 }).withMessage('El nombre debe tener al menos dos caracteres'),
-  body('last_name').notEmpty().withMessage("El apellido es obligatorio").isLength({ min: 2 }).withMessage('El apellido debe tener al menos dos caracteres'),
+	body('last_name').notEmpty().withMessage("El apellido es obligatorio").isLength({ min: 2 }).withMessage('El apellido debe tener al menos dos caracteres'),
 	body('email').notEmpty().withMessage("El email es obligatorio")
-	.isEmail().withMessage('Tienes que escribir un correo electrónico válido').bail()
+		.isEmail().withMessage('Tienes que escribir un correo electrónico válido').bail()
 		.isEmail().withMessage('Debes escribir un formato de correo válido'),
 	body('password').notEmpty().withMessage("La contraseña es obligatoria").isLength({ min: 8 }).withMessage('Tienes que escribir una contraseña de al menos 8 caracteres'),
 	body('phone').notEmpty().withMessage('Tienes que colocar tu número de teléfono'),
-  body('date_of_birth').notEmpty().withMessage('Tienes que colocar'),
-  body('home_adress').notEmpty().withMessage('Tienes que colocar tu dirección'),
+	body('date_of_birth').notEmpty().withMessage('Tienes que colocar'),
+	body('home_adress').notEmpty().withMessage('Tienes que colocar tu dirección'),
 	body('image').custom((value, { req }) => {
 		let file = req.file;
-		let acceptedExtensions = ['.jpg','.jpeg', '.png', '.gif'];
+		let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
 
 		if (!file) {
 			throw new Error('Tienes que subir una imagen de formato JPG, JPEG, PNG, GIF');
